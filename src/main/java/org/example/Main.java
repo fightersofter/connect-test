09b9c72b6e7758d7da055a03d322c7e9c18ee83e
wangpeng14721 <wangpeng14721@163.com>
2023-06-09 21:14:35 +0800

    aaaa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world，max!");
+        System.out.println("Hello world，max1!");
     }
 }
